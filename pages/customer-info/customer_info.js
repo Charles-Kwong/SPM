0e354d6940d171e05f30c35beb027685ba93eda0
Charles Kwong <itgoesback@126.com>
2019-04-21 02:48:11 +0800

    02:47

--- pages/customer-info/customer_info.js
+++ pages/customer-info/customer_info.js
@@ -1,62 +1,90 @@
 // pages/customer-info/customer_info.js
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    currentIndex: 0,
-    customer: {
-      id: "001",
-      name: "杨美丽",
-      gender: "女",
-      age: 25,
-      phone: "13288614700",
-      wechatId: "yangmeili",
-      addr: "广州市天河区龙洞",
-      type: "试用阶段",
-      startDate: "2019-04-01",
-      advisor: "张三",
-      skinDesc: "干性皮肤，有色斑。。。",
-      product: "洗面奶，爽肤水，保湿霜"
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        currentIndex: 0,
+        customer: {
+            id: "001",
+            name: "杨美丽",
+            gender: "女",
+            age: 25,
+            phone: "13288614700",
+            wechatId: "yangmeili",
+            addr: "广州市天河区龙洞",
+            type: "试用阶段",
+            startDate: "2019-04-01",
+            advisor: "张三",
+            skinDesc: "干性皮肤，有色斑。。。",
+            product: "洗面奶，爽肤水，保湿霜"
+        },
+        tracingRecord: [{
+            date: "2019-04-01",
+            desc: "干燥脱皮，泛红",
+            adjustment: "无",
+            advisor: "张三"
+        }]
     },
-    tracingRecord: [{
-      date: "2019-04-01",
-      desc: "干燥脱皮，泛红",
-      adjustment: "无",
-      advisor: "张三"
-    }]
-  },
-  //swiper切换时会调用
-  pagechange: function(e) {
-    if ("touch" === e.detail.source) {
-      let currentPageIndex = this.data.currentIndex
-      currentPageIndex = (currentPageIndex + 1) % 2
-      this.setData({
-        currentIndex: currentPageIndex
-      })
-    }
-  },
-  //用户点击tab时调用
-  titleClick: function(e) {
-    let currentPageIndex =
-      this.setData({
-        //拿到当前索引并动态改变
-        currentIndex: e.currentTarget.dataset.idx
-      })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
+    //swiper切换时会调用
+    pagechange: function(e) {
+        if ("touch" === e.detail.source) {
+            let currentPageIndex = this.data.currentIndex
+            currentPageIndex = (currentPageIndex + 1) % 2
+            this.setData({
+                currentIndex: currentPageIndex
+            })
+        }
+    },
+    //用户点击tab时调用
+    titleClick: function(e) {
+        let currentPageIndex =
+            this.setData({
+                //拿到当前索引并动态改变
+                currentIndex: e.currentTarget.dataset.idx
+            })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        Date.prototype.toLocaleString = function() {
+            function addZero(num) {
+                if (num < 10)
+                    return "0" + num;
+                return num;
+            }
+            return this.getFullYear() + "/" + addZero(this.getMonth() + 1) + "/" + addZero(this.getDate());
+        };
 
-  },
-  adddetial: function() {   
-    wx.navigateTo({ 
-      url: '/pages/customer-info/add_trace/add_trace',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
+        var that = this;
+        this.setData({
+            cliId: options.cliId
+        })
+        wx.request({
+            url: app.globalData.url.url + '/client/' + that.data.cliId,
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'cookie': wx.getStorageSync("cookies")
+            },
+            success: function(res) {
+                var customer = res.data;
+                customer.cliPurchaseDate = new Date(customer.cliPurchaseDate).toLocaleString();
+                //更新数据
+                that.setData({
+                    customer: customer
+                })
+            }
+        })
+    },
+    adddetial: function() {   
+        wx.navigateTo({ 
+            url: '/pages/customer-info/add_trace/add_trace',
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
+        })
+    },
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,114 +1,137 @@
 // pages/login/login.js
 const app = getApp();
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    disabled: false,
-    no: '',
-    pwd: '',
-    noinput: false,
-    pwdinput: false,
-    items: [
-      { name: 'manager', value: '店长' },
-      { name: 'employee', value: '美容导师', checked: 'true' },
-      { name: 'admin', value: '管理员' }
-    ]
-  },
-  radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
-  },
-  noinput: function (e) {
-    this.setData({ no: e.detail.value });
-    this.setData({ noinput: true });
-    if (this.data.noinput == true && this.data.pwdinput == true) {
-      this.setData({ disabled: false });
-    }
-
-  },
-  pwdinput: function (e) {
-    this.setData({ pwd: e.detail.value });
-    this.setData({ pwdinput: true });
-    if (this.data.noinput == true && this.data.pwdinput == true) {
-      this.setData({ disabled: false });
-    }
-  },
-  formSubmit: function (e) {
-    wx.showLoading({
-      title: '登录中...',
-    })
-    console.log(e);
-    this.setData({ disabled: true });
-    wx.request({
-      url: app.globalData.url.login, //仅为示例，并非真实的接口地址
-      data: {
-        no: e.detail.value.no,
-        pwd: e.detail.value.pwd
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res);
-        if (res.statusCode == 200) {
-          if (res.data.error == true) {
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'none',
-              duration: 2000
-            })
-          } else {
-            wx.setStorageSync('student', res.data.data);
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'success',
-              duration: 2000
-            })
-            setTimeout(function () {
-              wx.switchTab({
-                url: '../teacher/teacher',
-              })
-            }, 2000)
-          }
-        } else {
-          wx.showToast({
-            title: '服务器出现错误',
-            icon: 'none',
-            duration: 2000
-          })
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        disabled: false,
+        no: 'tutor',
+        pwd: '***',
+        noinput: false,
+        pwdinput: false,
+        items: [{
+                name: 'manager',
+                value: '店长'
+            },
+            {
+                name: 'employee',
+                value: '美容导师',
+                checked: 'true'
+            },
+            {
+                name: 'admin',
+                value: '管理员'
+            }
+        ]
+    },
+    radioChange: function(e) {
+        console.log('radio发生change事件，携带value值为：', e.detail.value)
+    },
+    noinput: function(e) {
+        this.setData({
+            no: e.detail.value
+        });
+        this.setData({
+            noinput: true
+        });
+        if (this.data.noinput == true && this.data.pwdinput == true) {
+            this.setData({
+                disabled: false
+            });
         }
-      }
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.setData({ disabled: false });
-    var student = wx.getStorageSync('student');
-    if (typeof (student) == 'object' && student.no != '' && student.classid != '') {
-      wx.switchTab({
-        url: '../teacher/teacher',
-      })
+
+    },
+    pwdinput: function(e) {
+        this.setData({
+            pwd: e.detail.value
+        });
+        this.setData({
+            pwdinput: true
+        });
+        if (this.data.noinput == true && this.data.pwdinput == true) {
+            this.setData({
+                disabled: false
+            });
+        }
+    },
+    formSubmit: function(e) {
+        wx.showLoading({
+            title: '登录中...',
+        })
+        this.setData({
+            disabled: true
+        });
+        wx.request({
+            method: 'post',
+            url: app.globalData.url.url + "/login", 
+            data: {
+                empName: e.detail.value.no,
+                empPassword: e.detail.value.pwd
+            },
+            header: {
+                'content-type': 'application/x-www-form-urlencoded' // 表单
+            },
+            success: function(res) {
+                if (res.statusCode == 200) {
+                    app.globalData.user = res.data;
+                    wx.setStorageSync('cookies', res.header["Set-Cookie"]);
+                    setTimeout(function() {
+                        wx.switchTab({
+                            url: '../index/index',
+                        })
+                    }, 1000)
+
+                } else if (res.statusCode == 401) {
+                    wx.showToast({
+                        title: '账号或密码错误',
+                        icon: 'none',
+                        duration: 1000
+                    })
+                } else {
+                    wx.showToast({
+                        title: '服务器出现错误',
+                        icon: 'none',
+                        duration: 2000
+                    })
+                }
+            }
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.setData({
+            disabled: false
+        });
+        var student = wx.getStorageSync('student');
+        if (typeof(student) == 'object' && student.no != '' && student.classid != '') {
+            wx.switchTab({
+                url: '../teacher/teacher',
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        if (this.data.no == '' || this.data.pwd == '') {
+            this.setData({
+                disabled: true
+            });
+        } else {
+            this.setData({
+                disabled: false
+            });
+        }
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    if (this.data.no == '' || this.data.pwd == '') {
-      this.setData({ disabled: true });
-    } else {
-      this.setData({ disabled: false });
-    }
-  }
 })
